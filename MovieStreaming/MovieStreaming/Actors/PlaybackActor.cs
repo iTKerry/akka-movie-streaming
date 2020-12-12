6589f8bfc97d5f7b559f2c207a3649fc9dc44dac
Alex K <a.katrynets@gmail.com>
2020-12-12 16:56:57 +0200

    add hooks

--- MovieStreaming/MovieStreaming/Actors/PlaybackActor.cs
+++ MovieStreaming/MovieStreaming/Actors/PlaybackActor.cs
@@ -8,14 +8,36 @@ namespace MovieStreaming.Actors
     {
         public PlaybackActor()
         {
-            Console.WriteLine($"Creating a {nameof(PlaybackActor)}.");
-            Receive<PlayMovieMessage>(HandlePlayMovieMessage, message => message.UserId == 42);
+            Console.WriteLine($"[{nameof(PlaybackActor)}] Creating...");
+            Receive<PlayMovieMessage>(HandlePlayMovieMessage);
         }
 
         private static void HandlePlayMovieMessage(PlayMovieMessage message)
         {
             var (userId, userName) = message;
             Console.WriteLine($"User ID: #{userId} and User Name: {userName}");
+        }
+
+        protected override void PreStart()
+        {
+            Console.WriteLine($"[{nameof(PlaybackActor)}] {nameof(PreStart)}");
+        }
+
+        protected override void PostStop()
+        {
+            Console.WriteLine($"[{nameof(PlaybackActor)}] {nameof(PostStop)}");
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            Console.WriteLine($"[{nameof(PlaybackActor)}] {nameof(PreRestart)} because: {reason}");
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            Console.WriteLine($"[{nameof(PlaybackActor)}] {nameof(PostRestart)} because: {reason}");
+            base.PostRestart(reason);
         }
     }
 }
--- MovieStreaming/MovieStreaming/Program.cs
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Sockets;
 using System.Threading.Tasks;
 using Akka.Actor;
 using MovieStreaming.Actors;
@@ -14,32 +13,17 @@ namespace MovieStreaming
         private static async Task Main()
         {
             _movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
-            Console.WriteLine("ActorSystem created.");
+            Console.WriteLine($"[{nameof(ActorSystem)}] created.");
             
             var playbackActorProps = Props.Create<PlaybackActor>();
             var playbackActorRef = _movieStreamingActorSystem.ActorOf(playbackActorProps, nameof(PlaybackActor));
             
             playbackActorRef.Tell(new PlayMovieMessage(1, "Akka.NET: THE MOVIE"));
-
-            var packet = new Packet(new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp));
             
-            playbackActorRef.Tell(packet);
+            await _movieStreamingActorSystem.Terminate();
+            Console.WriteLine($"{nameof(ActorSystem)} terminated.");            
             
             Console.Read();
-
-            await _movieStreamingActorSystem.Terminate();
-        }
-    }
-    
-    public class Packet
-    {
-        public int Pos { get; private set; } = 0;
-        public byte[] Buf { get; set; } = new byte[1460];
-        public Socket Socket { get; private set; }
-        
-        public Packet(Socket socket)
-        {
-            Socket = socket;
         }
     }
 }
